e2845bba00b88560544ef307d37b785b5f123428
danilshabolta <danilshabolta@gmail.com>
2024-11-24 18:54:02 +0700

    add: добавлены store и slice

---
+++ src/components/redux/todoSlice.js
@@ -0,0 +1,21 @@
+// slices/todoSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState: [],
+  reducers: {
+    addTodo: (state, action) => {
+      state.push(action.payload);
+    },
+    removeTodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload.id);
+    },
+    setTodos: (state, action) => {
+      return action.payload;
+    },
+  },
+});
+
+export const { addTodo, removeTodo, setTodos } = todoSlice.actions;
+export default todoSlice.reducer;
